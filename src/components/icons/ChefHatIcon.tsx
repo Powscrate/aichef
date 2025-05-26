eade328a79d547e44db550061f6b3b54c79d374c
Tantely Gasikara <tantelygasikara@gmail.com>
2025-05-26 21:58:25 +0000

    Update Logo and Favicon

--- src/components/icons/ChefHatIcon.tsx
+++ src/components/icons/ChefHatIcon.tsx
@@ -7,19 +7,22 @@ export function ChefHatIcon(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
-      viewBox="0 0 24 24"
+      viewBox="0 0 24 24" // Adjusted viewBox to provide more space for detail
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
+ // Added a title for accessibility
+
     >
-      <path d="M19.5 12.5c0-2.5-1.42-4.7-3.5-5.72V3.5C16 2.67 15.33 2 14.5 2h-5C8.67 2 8 2.67 8 3.5v3.28c-2.08 1.02-3.5 3.22-3.5 5.72C4.5 17 8 17 12 17s7.5 0 7.5-4.5z" />
-      <path d="M8.5 8.5c0-1.42.5-2.78 1.39-3.82" />
-      <path d="M14.11 4.68C15 5.72 15.5 7.08 15.5 8.5" />
-      <path d="M12 17v5" />
-      <path d="M9 22h6" />
+
+      <title>Chef Hat</title>
+      {/* Main hat shape - More detailed curve for a realistic look */}
+      <path d="M4 18c0-4.41 3.59-8 8-8s8 3.59 8 8v2H4v-2zM10 10v-6h4v6M8 18v2h8v-2c0-2.21-1.79-4-4-4s-4 1.79-4 4z" />
+      {/* Simple lines for texture or folds */}
+      <path d="M6 18s1-2 2-2 2 2 4 2 2-2 4-2 2 2 2 2" opacity=".5"/>
     </svg>
   );
 }
